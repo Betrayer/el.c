832f63f8e3cfea04586db579d34fc2df5badc7f8
Betrayer <StarDestroyer@ukr.net>
2020-06-15 17:37:08 +0300

    init block

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <h2>i am header</h2>
+    </>
+  );
+};
+
+export default Header;
