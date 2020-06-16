3d43bfe877b62db80566871f671639f6ca60c66e
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-06-16 10:10:44 +0300

    basic styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Blog from './components/blog/Blog.js'
+import './app.module.css'
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      {/* <Header /> */}
+      <Blog />
+      {/* <Footer /> */}
     </>
   );
 }
