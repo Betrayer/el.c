16c69fec50fc626d0f7ff437051a8045daaf254c
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-06-16 11:57:05 +0300

    some basic style and begging of blog

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 // import { withRouter, useHistory } from "react-router-dom";
+import FB from 'fb'
 import styles from './blog.module.css'
 
 const Blog = () => {
+    const [posts, setPosts] = useState([])
+    const a = FB.api(
+        "/{page-id}/feed",
+        function (response) {
+          if (response && !response.error) {
+            setPosts(response)
+          }
+        }
+    );
     return (
         <div className={styles.bg}>
             <div>Header</div>
             <div className={styles.mainImg}></div>
             <div className={styles.container}>
-            <div className={styles.postGrid}></div>
+                
+            <div className={styles.postGrid}>
+
+            </div>
             </div>
             <div>footer</div>
         </div>
