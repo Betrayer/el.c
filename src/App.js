7e40acc9bab2fac89269211ee80252f20da528b7
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-06-16 21:05:59 +0300

    blog from main html/css done or so

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Blog from './components/blog/Blog.js'
-import './app.module.css'
 import Services from "./components/services/Services";
+import MainBlog from "./components/mainBlog/MainBlog";
+import './app.module.css'
 
 function App() {
   return (
     <>
       {/* <Header /> */}
       <Services />
-      <Blog />
+      {/* <Blog /> */}
+      <MainBlog />
       {/* <Footer /> */}
     </>
   );
