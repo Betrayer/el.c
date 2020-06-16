ff134885cf9d18c7906c4adfe97e2da70eb97caa
Roman Verzhbovskyi <58361292+romanitwork@users.noreply.github.com>
2020-06-16 13:48:26 +0300

    Services (#5)
    * bogdan is a cock
    
    * start services
    
    * start working on services

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Blog from './components/blog/Blog.js'
 import './app.module.css'
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <>
       {/* <Header /> */}
+      <Services />
       <Blog />
       {/* <Footer /> */}
     </>
---
+++ src/components/services/Services.js
@@ -0,0 +1,74 @@
+import React from "react";
+import css from "./Services.module.css";
+
+const Services = () => {
+  return (
+    <>
+      <h2 className={css.services_title}>Наши услуги</h2>
+      <ul className={css.services_list}>
+        <li className={css.services_item}>
+          <h3 className={css.services_item_title}>Title1</h3>
+          <span className={css.services_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </li>
+        <li className={css.services_item}>
+          <h3 className={css.services_item_title}>Title2</h3>
+          <span className={css.services_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </li>
+        <li className={css.services_item}>
+          <h3 className={css.services_item_title}>Title3</h3>
+          <span className={css.services_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </li>
+        <li className={css.services_item}>
+          <h3 className={css.services_item_title}>Title4</h3>
+          <span className={css.services_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </li>
+        <li className={css.services_item}>
+          <h3 className={css.services_item_title}>Title5</h3>
+          <span className={css.services_text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Services;
