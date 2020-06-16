2927be3ba68294142151ffeb72308fee2869a42c
romanitwork <workromanmail@gmail.com>
2020-06-16 11:46:14 +0300

    start services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <>
       <Header />
+      <Services />
       <Footer />
     </>
   );
