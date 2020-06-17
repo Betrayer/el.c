4836aaccb653804d5d0595e553b015a54e3b82c4
romanitwork <workromanmail@gmail.com>
2020-06-17 10:12:19 +0300

    ver with main

---
+++ src/components/main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../header/Header'
+import Services from '../services/Services'
+import MainBlog from '../mainBlog/MainBlog'
+
+
+const Main = () => {
+    return (
+        <>
+        <Header/>
+        <Services/>
+        <MainBlog/>
+        </>
+    );
+}
+
+export default Main;
