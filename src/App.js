725b8358d6a1f72d0364bb6ef395c32c13d8be51
Ellie Yelizarieva <ellie@Ellies-MacBook-Pro.local>
2020-06-17 22:34:26 +0300

    more like a template

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       {/* <Header /> */}
     {/* <ExtendedMenu /> */}
     <Main/>
-      <Services />
+      {/* <Services /> */}
       {/* <Blog /> */}
       {/* <MainBlog /> */}
       {/* <Footer /> */}
