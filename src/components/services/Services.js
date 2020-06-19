fc12efbc80d88021aed13585fe3e06bb105cfbab
Roman Verzhbovskyi <58361292+romanitwork@users.noreply.github.com>
2020-06-19 10:22:31 +0300

    Services (#22)
    * bogdan is a cock
    
    * start services
    
    * start working on services
    
    * start services styles
    
    * mobile services
    
    * ver with main
    
    * commit for pull
    
    * services alpha after pull

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import TextTransition, { presets } from "react-text-transition";
 import css from "./Services.module.css";
 
-const TEXTS = ["Наши услуги", "Что мы можем"];
+const TEXTS = ["Услуги", "Сервис", "Поддержка"];
 
 const Services = () => {
   const [index, setIndex] = useState(0);
+  const [arrow, setArrow] = useState(false);
 
   useEffect(() => {
     indexChange();
@@ -35,6 +36,26 @@ const Services = () => {
         />
         <ul className={css.services_list}>
           <li className={css.services_box1}>
+          <img
+          className={css.services_dot1}
+          src={require("../../assets/photos/services_dot.png")}
+          alt="services_line"
+        />
+         <img
+          className={css.services_dot2}
+          src={require("../../assets/photos/services_dot.png")}
+          alt="services_line"
+        />
+          <img
+          className={css.services_line1}
+          src={require("../../assets/photos/services_line.png")}
+          alt="services_line"
+        />
+         <img
+          className={css.services_line2}
+          src={require("../../assets/photos/services_line.png")}
+          alt="services_line"
+        />
             <ul className={css.services_box1_list}>
               <li className={css.services_box1_item}>
                 <h3 className={css.services_item_title}>Title1</h3>
@@ -57,6 +78,26 @@ const Services = () => {
             </ul>
           </li>
           <li className={css.services_box2}>
+          <img
+          className={css.services_dot3}
+          src={require("../../assets/photos/services_dot.png")}
+          alt="services_line"
+        />
+         <img
+          className={css.services_dot4}
+          src={require("../../assets/photos/services_dot.png")}
+          alt="services_line"
+        />
+          <img
+          className={css.services_line3}
+          src={require("../../assets/photos/services_line.png")}
+          alt="services_line"
+        />
+         <img
+          className={css.services_line4}
+          src={require("../../assets/photos/services_line.png")}
+          alt="services_line"
+        />
             <ul className={css.services_box2_list}>
               <li className={css.services_box2_item}>
                 <h3 className={css.services_item_title}>Title3</h3>
@@ -79,6 +120,16 @@ const Services = () => {
             </ul>
           </li>
           <li className={css.services_box3}>
+          <img
+          className={css.services_line5}
+          src={require("../../assets/photos/services_line.png")}
+          alt="services_line"
+        />
+         <img
+          className={css.services_dot5}
+          src={require("../../assets/photos/services_dot.png")}
+          alt="services_line"
+        />
             <h3 className={css.services_item_title}>Title5</h3>
             <p className={css.services_text}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -88,6 +139,20 @@ const Services = () => {
             </p>
           </li>
         </ul>
+        <button
+          onMouseOver={() => setArrow(true)}
+          onMouseLeave={() => setArrow(false)}
+          className={css.learnMore}
+        >
+          <span className={css.circle} aria-hidden="true">
+            {arrow ? (
+              <span className={css.arrow}></span>
+            ) : (
+              <span className={css.icon}></span>
+            )}
+          </span>
+          <span className={css.buttonText}>узнать подробнее</span>
+        </button>
       </div>
     </section>
   );
