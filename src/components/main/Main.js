1b554e355ca42ddc86679cf6bc17709665c586ae
Alexlogvin2019 <57359889+Alexlogvin2019@users.noreply.github.com>
2020-06-19 15:17:47 +0300

    ver1 (#30)

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 import Header from "../header/Header";
 import Services from "../services/Services";
 import MainBlog from "../mainBlog/MainBlog";
 import Footer from "../footer/Footer";
 import Contacts from "../contacts/Contacts";
+import Form from "../form/Form.js";
 
-const Main = () => {
+const Main = (props) => {
   return (
     <>
       <Header />
+      <Form env={props.env} />
       <Services />
       <MainBlog />
       <Contacts />
@@ -17,5 +20,9 @@ const Main = () => {
     </>
   );
 };
+Main.propTypes = {
+  env: PropTypes.object.isRequired,
+};
+
 
 export default withRouter(Main);
