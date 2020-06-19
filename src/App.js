3ae9baabc86246f463e9a310c77b87292c0cfd66
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-06-19 14:52:18 +0300

    ver1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Blog from './components/blog/Blog.js'
-import './app.module.css'
+import Blog from "./components/blog/Blog.js";
+import Form from "./components/form/Form";
+import PropTypes from "prop-types";
 
-function App() {
+import "./app.module.css";
+
+function App(props) {
   return (
     <>
       {/* <Header /> */}
       <Blog />
+      <Form env={props.env} />
       {/* <Footer /> */}
     </>
   );
 }
+App.propTypes = {
+  env: PropTypes.object.isRequired,
+};
 
 export default App;
