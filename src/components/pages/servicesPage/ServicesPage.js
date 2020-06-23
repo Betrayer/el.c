d3662d255437b85e0cbeaf703d7de232f9ca49fd
Betrayer <StarDestroyer@ukr.net>
2020-06-23 16:32:05 +0300

    button fix in about section

--- src/components/pages/servicesPage/ServicesPage.js
+++ src/components/pages/servicesPage/ServicesPage.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
 const ServicesPage = () => {
-    return (
-        <>
-        <p>I am ServicesPage</p>
-        </>
-    );
-}
+  return (
+    <>
+      <p>I am ServicesPage</p>
+    </>
+  );
+};
 
-export default ServicesPage;
+export default withRouter(ServicesPage);
